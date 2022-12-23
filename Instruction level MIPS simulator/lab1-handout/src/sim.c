bc55b7ec2e2448f4408954094e24f48ad9fc6d4f
AmitPatilTamu <120693923+AmitPatilTamu@users.noreply.github.com>
2022-12-22 19:23:41 -0600

    Update sim.c

--- Instruction level MIPS simulator/lab1-handout/src/sim.c
+++ Instruction level MIPS simulator/lab1-handout/src/sim.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 #include "shell.h"
 
+#define SRLV 000110
+#define SUB 100010
+#define SLT 101010
+#define MTLO 010011
+#define SRAV 000111
+#define SUBU 100011
+#define SLTU 101011
+#define MULTU 011001
+#define SLL 000000
+#define JR 001000
+#define AND 100100
+#define MULT 011000
+#define DIV 011010
+#define DIVU 011011
+#define SRL 000010
+#define SRLV 000010
+#define JALR 001001
+#define OR 100101
+#define SRA 000011
+#define ADD 100110
+#define XOR 100110
+#define MFLO 010010
+#define SYSCALL 001100
+#define NOR 100111
+#define 
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+	
+
 void process_instruction()
 {
     /* execute one instruction here. You should use CURRENT_STATE and modify
@@ -22,8 +62,37 @@ void process_instruction()
 }
 
 void Execute_R_type(uint32_t curr_ins) {
+    uint32_t func = curr_ins & 0x3f;
+    uint32_t rs = (curr_ins << 6) >> 27;
+    uint32_t rt = (curr_ins << 11) >> 27;
+    uint32_t rd = (curr_ins << 16) >> 27;
+    uint32_t shamt = (curr_ins << 21) >> 27;
+	
+ switch (func) {
+	case SRLV: NEXT_STATE.REGS[rd]=CURRENT_STATE.REGS[rt]>>CURRENT_STATE.REGS[rs]; break;
+	case SUB: NEXT_STATE.REGS[rd]=CURRENT_STATE.REGS[rs] - CURRENT_STATE.REGS[rt]; break;
+	case SLT: NEXT_STATE.REGS[rd]=((int32_t)CURRENT_STATE.REGS[rs] < (int32_t)CURRENT_STATE.REGS[rt])?1:0; break;
+	case MTLO: NEXT_STATE.LO=CURRENT_STATE.REGS[rs]; break;
+	case SRAV: NEXT_STATE.REGS[rd]=(int32_t)CURRENT_STATE.REGS[rt]>>((CURRENT_STATE.REGS[rs]<<27)>>27); break;
+	case SUBU: NEXT_STATE.REGS[rd]=CURRENT_STATE.REGS[rs] - CURRENT_STATE.REGS[rt]; break;
+	case SLTU: NEXT_STATE.REGS[rd]=(CURRENT_STATE.REGS[rs] < CURRENT_STATE.REGS[rt])?1:0; break;
+	case MULTU: int64_t product = CURRENT_STATE.REGS[rs]*CURRENT_STATE.REGS[rt];NEXT_STATE.HI=product>>32; NEXT_STATE.LO=product;break;
+	case SLL: NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rt] << shamt; break;
+	case JR: NEXT_STATE.PC = CURRENT_STATE.REGS[rs]; break;
+	case MULT: NEXT_STATE.REGS[rd]=(int32_t)CURRENT_STATE.REGS[rs] * (int32_t)CURRENT_STATE.REGS[rt]; break;
+	case DIV: NEXT_STATE.HI = (int32_t)CURRENT_STATE.REGS[rs] % (int32_t)CURRENT_STATE.REGS[rt]; NEXT_STATE.LO = (int32_t)CURRENT_STATE.REGS[rs] / (int32_t)CURRENT_STATE.REGS[rt]; break;
+	case DIVU: NEXT_STATE.HI = CURRENT_STATE.REGS[rs] % CURRENT_STATE.REGS[rt]; NEXT_STATE.LO = CURRENT_STATE.REGS[rs] / CURRENT_STATE.REGS[rt]; break;
+	case SRL: NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rt] >> shamt; break;
+	case SRLV: NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rt] >> ((CURRENT_STATE.REGS[rs] << 27) >> 27); break;
+	case JALR: NEXT_STATE.PC = CURRENT_STATE.REGS[rs];NEXT_STATE.REGS[rd] = CURRENT_STATE.PC + 4; break;
+	case OR: NEXT_STATE.REGS[rd]=CURRENT_STATE.REGS[rs] | CURRENT_STATE.REGS[rt]; break;
+	case SRA: NEXT_STATE.REGS[rd]=CURRENT_STATE.REGS[rs] - CURRENT_STATE.REGS[rt]; break;
+	case ADD: NEXT_STATE.REGS[rd]=CURRENT_STATE.REGS[rs] - CURRENT_STATE.REGS[rt]; break;
+	case XOR: NEXT_STATE.REGS[rd]=CURRENT_STATE.REGS[rs] - CURRENT_STATE.REGS[rt]; break;
+	case MFLO: NEXT_STATE.REGS[rd]=CURRENT_STATE.REGS[rs] - CURRENT_STATE.REGS[rt]; break;
+	case SYSCALL: NEXT_STATE.REGS[rd]=CURRENT_STATE.REGS[rs] - CURRENT_STATE.REGS[rt]; break;
+	
 
-	switch()
-
+	
 
 }
